f94e9aa587e598b654f9d5dd9116e96a3f3dcff1
Bright Delali-Gagloh <48211431+tearfas@users.noreply.github.com>
2019-11-10 23:13:09 -0500

    Add files via upload

---
+++ agecal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#########THIS SCRIPT CAN HELP DETERMINE YOUR AGE #########
+
+read -p "What is your name please?" name
+
+read -p "What company do you work for?" company
+
+read -p "What year were you born ?" year
+
+read -p "Please provide your birth month Example 06" month
+
+curyear=$(date +%Y)
+curmonth=$(date +%m)
+
+ageinyrs=$(($curyear-$year))
+ageinmnt=$(($curmonth-$month))
+
+if [ $ageinmnt -lt 0 ]; then
+	ageinmnt=$(($curmonth-$month+12))
+	ageinyrs=$((ageinyrs-1))
+else
+	ageinmnt=$(($curmonth-$month))
+fi
+
+echo "Hello mr/mrs $name who warks at $company you are $ageinyrs years and $ageinmnt months old"
